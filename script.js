1b332268b05d2f7247d9903a32fd97b344da7290
WeTop0714 <editz.me0312@gmail.com>
2026-07-26 10:24:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+// =========================
+// WeTop v3 JavaScript
+// =========================
 
+// Shopping cart
+let cart = [];
+
+// Add product to cart
+function addToCart(name, price){
+
+cart.push({
+name:name,
+price:price
+});
+
+alert("✅ " + name + " added to cart!");
+
+console.log(cart);
+
+}
+
+// Wishlist
+let wishlist = [];
+
+function addToWishlist(name){
+
+wishlist.push(name);
+
+alert("❤️ " + name + " added to wishlist!");
+
+console.log(wishlist);
+
+}
